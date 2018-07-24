ecb77b6136d29ad214c62566e170769e96be9b0d
unknown <you@example.com>
2018-07-25 00:20:34 +0800

    src/

--- src/main/java/com/liyuhang/controller/ServerController.java
+++ src/main/java/com/liyuhang/controller/ServerController.java
@@ -1,5 +1,19 @@
 package com.liyuhang.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * 服务controller
+ * 
+ * @author LiYuHang
+ * @version 1.0
+ * @since 2018/07/24
+ */
+@Controller
+@RequestMapping
 public class ServerController
 {
 
---
+++ src/main/java/com/liyuhang/model/User.java
@@ -0,0 +1,13 @@
+package com.liyuhang.model;
+
+/**
+ * 框架测试model类
+ * 
+ * @author LiYuHang
+ * @version 1.0
+ * @since 2018/07/24
+ */
+public class User
+{
+
+}
