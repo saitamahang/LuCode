a159958a39dc630d9a6e16ae5e5609c3127e41db
liyuhang <xbypdcts@outlook.com>
2018-11-15 23:05:44 +0800

    hash.java

---
+++ src/main/java/com/liyuhang/java/Hash/hash.java
@@ -0,0 +1,18 @@
+package com.liyuhang.java.Hash;
+
+public class hash
+{
+    @Override
+    public int hashCode()
+    {
+        // TODO Auto-generated method stub
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+}
