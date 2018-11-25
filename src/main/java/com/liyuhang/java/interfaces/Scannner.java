00edbe85d23d9345018260a764965f96f806c643
liyuhang <xbypdcts@outlook.com>
2018-11-26 00:01:30 +0800

    接口 适配器模式测试类

--- src/main/java/com/liyuhang/java/interfaces/Scannner.java
+++ src/main/java/com/liyuhang/java/interfaces/Scannner.java
@@ -26,9 +26,7 @@ public class Scannner
 
 
 /**
- * 适配器 
- * 
- * AdaptedRandomDoubles实现Readable接口，适配Scanner
+ * 适配器 AdaptedRandomDoubles实现Readable接口，适配Scanner
  * 
  * @author LiYuHang
  */
@@ -60,9 +58,18 @@ class AdaptedRandomDoubles implements Readable
             return -1;
         }
         String result = Double.toString(next()) + "";
-        cb.append(result);
+        cb.append(result);// ???此处cb为空
         System.out.println(result + " ============= " + cb);
         return result.length();
     }
 
 }
+// out rsult:
+//
+// 0.7271157860730044 =============
+// 0.5309454508634242 =============
+// 0.16020656493302599 =============
+// 0.18847866977771732 =============
+// 0.5166020801268457 =============
+// 0.2678662084200585 =============
+// 0.2613610344283964 =============
