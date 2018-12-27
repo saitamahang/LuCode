d1406b2b99b67b9848b6aee7611a518cb5c2c9d1
liyuhang <xbypdcts@outlook.com>
2018-12-27 21:20:49 +0800

    无限递归调用

---
+++ src/main/java/com/liyuhang/java/String/InfiniterRecursion.java
@@ -0,0 +1,34 @@
+package com.liyuhang.java.String;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * 无限递归调用
+ * 
+ * @author LiYuHang
+ * @date 2018年12月27日
+ */
+public class InfiniterRecursion
+{
+    @Override
+    public String toString()
+    {
+        // 无限递归调用
+        // "===============" + this + "+++++++++++";
+        return "===============" + super.toString() + "+++++++++++";
+    }
+
+    public static void main(String[] args)
+    {
+        List<InfiniterRecursion> iinR = new ArrayList<InfiniterRecursion>();
+        for (int i = 0; i < 1; i++ )
+        {
+            iinR.add(new InfiniterRecursion());
+        }
+
+        System.out.println(iinR);
+    }
+}
