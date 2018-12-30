9dc8ce53606aa56567f453ed4bea30c9ff291524
liyuhang <xbypdcts@outlook.com>
2018-12-30 14:45:12 +0800

    类型信息测试

---
+++ src/main/java/com/liyuhang/java/ClassInfo/ClassObject.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.liyuhang.java.ClassInfo;
+
+/**
+ * @author LiYuHang
+ * @date 2018年12月30日
+ */
+public class ClassObject
+{
+
+}
--- src/main/java/com/liyuhang/java/ClassInfo/ClassTest.java
+++ src/main/java/com/liyuhang/java/ClassInfo/ClassTest.java
@@ -3,6 +3,10 @@
  */
 package com.liyuhang.java.ClassInfo;
 
+
+import java.util.Random;
+
+
 /**
  * 类型信息测试
  * 
@@ -11,7 +15,11 @@ package com.liyuhang.java.ClassInfo;
  */
 class Initable
 {
+    static Random random = new Random(1000);
+
     static final int staticFinal = 47;
+
+    static final int staticFinal2 = random.nextInt(100);
 
     // static final int staticFinal =
     Initable()
@@ -55,12 +63,14 @@ public class ClassTest
         System.out.println("-----------------------   分割线    --------------------");
         System.out.println(".calss 不会初始化类内部静态成员");
 
-        // 引用静态变量，似乎并不会使类内部的静态成员初始化
+        // 如果类引用“编译期常量”，并不会使类内部的静态成员初始化 eg:Initable.staticFinal
+        // 如果只是将一个域设置为static final 则需要初始化这个类，所以静态成员也会初始化 eg:Initable.staticFinal2
         // new 操作 、.forName才会初始化
-        System.out.println(Initable.staticFinal);
+        System.out.println(Initable.staticFinal2);
 
         Class initab2 = Class.forName("com.liyuhang.java.ClassInfo.Initable2");
-        test();
+        // test();
+        // Class initabb = Class.forName("com.liyuhang.java.ClassInfo.Initable");
     }
 
     public static void test()
