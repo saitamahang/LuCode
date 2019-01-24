5cc3b9b74f7d5ec3f517e3a40f76d3a8ac31ae66
liyuhang <xbypdcts@outlook.com>
2019-01-24 22:39:57 +0800

    反射

--- src/main/java/com/liyuhang/java/RTTI/ReflectTest.java
+++ src/main/java/com/liyuhang/java/RTTI/ReflectTest.java
@@ -24,11 +24,31 @@ public class ReflectTest
         {
             System.out.println(method.toString());
         }
-
+            System.out.println("======================================================");
         Constructor[] cons = clazz.getConstructors();
         for (Constructor constructor : cons)
         {
             System.out.println(constructor.toString());
         }
     }
+
+    /**
+     * 外部命令运行：
+     * 1. javac -d . ReflectTest.java 
+     * 2. java com.liyuhang.java.RTTI.ReflectTest com.liyuhang.java.RTTI.ReflectTest
+     * 
+     * Output:
+     *  public static void com.liyuhang.java.RTTI.ReflectTest.main(java.lang.String[]) throws java.lang.Exception
+        public final void java.lang.Object.wait() throws java.lang.InterruptedException
+        public final void java.lang.Object.wait(long,int) throws java.lang.InterruptedException
+        public final native void java.lang.Object.wait(long) throws java.lang.InterruptedException
+        public boolean java.lang.Object.equals(java.lang.Object)
+        public java.lang.String java.lang.Object.toString()
+        public native int java.lang.Object.hashCode()
+        public final native java.lang.Class java.lang.Object.getClass()
+        public final native void java.lang.Object.notify()
+        public final native void java.lang.Object.notifyAll()
+        ======================================================
+        public com.liyuhang.java.RTTI.ReflectTest()
+     */
 }
