bf2180ee157d44cdf7d5fc8b69d53ddd67ca9182
liyuhang <xbypdcts@outlook.com>
2019-01-30 23:37:50 +0800

    proxy 简单动态代理实现

--- src/main/java/com/liyuhang/java/RTTI/SimpleDynamicProxy.java
+++ src/main/java/com/liyuhang/java/RTTI/SimpleDynamicProxy.java
@@ -6,21 +6,25 @@ package com.liyuhang.java.RTTI;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 
 /**
  * @author LiYuHang
  * @date 2019年1月28日
  */
-public class SimpleDynamicProxy
-{
-
-}
-
-
 class DynamicProxyHandler implements InvocationHandler
 {
+    private realObject obj = new realObject();
 
+    /**
+     * 代理处理器
+     */
+    public DynamicProxyHandler(realObject obj)
+    {
+        this.obj = obj;
+        // TODO Auto-generated constructor stub
+    }
     /*
      * (non-Javadoc)
      * @see java.lang.reflect.InvocationHandler#invoke(java.lang.Object, java.lang.reflect.Method,
@@ -30,8 +34,60 @@ class DynamicProxyHandler implements InvocationHandler
     public Object invoke(Object proxy, Method method, Object[] args)
         throws Throwable
     {
+        if (null != args)
+        {
+            for (Object org : args)
+            {
+                System.out.println("    " + org);
+            }
+        }
+
+        System.out.println("================ 代理前 ==================");
+        Object proxyyy = method.invoke(obj, args);
+        System.out.println("================ 代理后 ==================");
+
+        return proxyyy;
+    }
+
+}
+
+
+public class SimpleDynamicProxy
+{
+
+    public static void main(String[] args)
+    {
+        ProxyInterface proxyInterface = (ProxyInterface)Proxy.newProxyInstance(
+            SimpleDynamicProxy.class.getClassLoader(), new Class[] {SimpleDynamicProxy.class},
+            new DynamicProxyHandler(new realObject()));
+
+        proxyInterface.methodOne();
+        proxyInterface.methodTwo();
+    }
+
+}
+
+
+class realObject implements ProxyInterface
+{
+
+    @Override
+    public void methodOne()
+    {
         // TODO Auto-generated method stub
-        return null;
+        System.out.println("This is method One!");
+    }
+
+    /**
+     * (non-Javadoc)
+     * 
+     * @see com.liyuhang.java.RTTI.ProxyInterface#methodTwo()
+     */
+    @Override
+    public void methodTwo()
+    {
+        // TODO Auto-generated method stub
+        System.out.println("This is method Two!");
     }
 
 }
