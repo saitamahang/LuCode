cb5ce5a4d3ff6ea371be5173bb8b2d0ba43e0cab
liyuhang <xbypdcts@outlook.com>
2019-08-28 22:24:24 +0800

    修改项目名称 增加servlet  pom依赖

---
+++ src/main/java/ServeletTest.java
@@ -0,0 +1,32 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author LiYuHang
+ * @date 2019年5月6日
+ */
+public class ServeletTest extends HttpServlet
+{
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
