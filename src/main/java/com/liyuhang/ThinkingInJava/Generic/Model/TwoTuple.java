6c7e94d7b6c853dc76d10861bf7fe943cd5dd3ae
liyuhang <xbypdcts@outlook.com>
2019-10-20 11:45:29 +0800

    泛型基础model类

---
+++ src/main/java/com/liyuhang/ThinkingInJava/Generic/Model/TwoTuple.java
@@ -0,0 +1,18 @@
+package com.liyuhang.ThinkingInJava.Generic.Model;
+
+/**
+ *  二元组
+ * @param <A>
+ * @param <B>
+ */
+public class TwoTuple<A, B> {
+    public final A first;
+    public final B second;
+    public  TwoTuple (A a, B b){
+        first = a;
+        second = b;
+    }
+    public String toString(){
+        return "(" + "first" + "," + second + ")" ;
+    }
+}
