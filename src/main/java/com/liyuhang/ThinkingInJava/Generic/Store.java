630b36a11113cd88dad6868ccb2e3395d2f87109
liyuhang <xbypdcts@outlook.com>
2019-10-20 21:07:22 +0800

    泛型练习

--- src/main/java/com/liyuhang/ThinkingInJava/Generic/Store.java
+++ src/main/java/com/liyuhang/ThinkingInJava/Generic/Store.java
@@ -54,9 +54,33 @@ class Aisle extends ArrayList<Shelf>{
     }
 }
 
-class  CheckoutStand {}
+class CheckoutStand {}
 class Office {}
 
 public class Store extends ArrayList<Aisle>{
+    private ArrayList<CheckoutStand> checkoutStands = new ArrayList<CheckoutStand>();
+    private Office office = new Office();
+    public Store(int nAisles, int nShelves, int nProducts){
+        for (int i = 0; i < nAisles; i++){
+            add(new Aisle(nShelves, nProducts));
+        }
+    }
 
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (Aisle a : this){
+            for (Shelf s : a){
+                for (Product p : s){
+                    result.append(p);
+                    result.append("\n");
+                }
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Store(14,5,10));
+    }
 }
