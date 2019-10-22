7ab1b41b3ffe513d85c4d6eaf078e58ae1f51726
liyuhang <liyuhang@kyee.com.cn>
2019-10-22 17:56:04 +0800

    泛型擦除缺陷的补偿

--- src/main/java/com/liyuhang/ThinkingInJava/Generic/GenericShotComing.java
+++ src/main/java/com/liyuhang/ThinkingInJava/Generic/GenericShotComing.java
@@ -14,3 +14,28 @@ public class GenericShotComing<T> {
         // T[] array = (T)new Object()[SIZE];
     }
 }
+
+/**
+ * 显式的传入类型的Class
+ * 可以解决上述类的泛型缺陷问题
+ * @param <T> 类型参数
+ */
+class ClassTypeCapture<T> {
+    Class<T> kind;
+    public ClassTypeCapture(Class<T> kind){
+        this.kind = kind;
+    }
+    public boolean f(Object org){
+        return kind.isInstance(org);
+    }
+
+    public static void main(String[] args) {
+        ClassTypeCapture<Building> ctt1 = new ClassTypeCapture<Building>(Building.class);
+        System.out.println(ctt1.f(new Building()));
+        System.out.println(ctt1.f(new House()));
+
+    }
+}
+
+class Building {}
+class House extends Building{}
