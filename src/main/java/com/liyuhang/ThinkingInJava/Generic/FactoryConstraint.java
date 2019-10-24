ceafdb54c694c987e2c73dea88ca31c81386f475
liyuhang <xbypdcts@outlook.com>
2019-10-24 23:11:09 +0800

    泛型工厂测试

--- src/main/java/com/liyuhang/ThinkingInJava/Generic/FactoryConstraint.java
+++ src/main/java/com/liyuhang/ThinkingInJava/Generic/FactoryConstraint.java
@@ -11,6 +11,9 @@ interface FactoryI<T>{
     T creat();
 }
 
-class Foo2{
-
+class Foo2<T>{
+    private T x;
+    public <F extends FactoryI<T>> Foo2(F factory){
+        x = factory.creat();
+    }
 }
