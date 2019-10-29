8f57652dd3e8ace792fecd2064fbda44c0667cb7
liyuhang <xbypdcts@outlook.com>
2019-10-29 23:28:09 +0800

    泛型类型跟踪

--- src/main/java/com/liyuhang/ThinkingInJava/Generic/ArrayGeneric.java
+++ src/main/java/com/liyuhang/ThinkingInJava/Generic/ArrayGeneric.java
@@ -9,6 +9,8 @@ public class ArrayGeneric {
         gia = (Generic<Integer>[])new Generic[SIZE];
         System.out.println(gia.getClass().getSimpleName());
         gia[0] = new Generic<Integer>();
+        // 虽然gia已经被转型为(Generic<Integer>[])，但这个信息只存在于编译期
+        // 运行时它仍为Object数组
         //! gia[1] = new Object();
         //! gia[2] = new Generic<>(Double);
     }
