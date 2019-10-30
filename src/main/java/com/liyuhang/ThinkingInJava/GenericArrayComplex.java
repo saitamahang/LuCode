8506a86c3c042d2b117087f595b83d0029354adb
liyuhang <xbypdcts@outlook.com>
2019-10-30 22:59:55 +0800

    泛型类型跟踪

--- src/main/java/com/liyuhang/ThinkingInJava/GenericArrayComplex.java
+++ src/main/java/com/liyuhang/ThinkingInJava/GenericArrayComplex.java
@@ -2,7 +2,20 @@ package com.liyuhang.ThinkingInJava;
 
 public class GenericArrayComplex<T> {
     private T[] array;
-    public GenericArrayComplex(int index){
+    public GenericArrayComplex(int sz){
+        array  = (T[])new Object[sz];
+    }
 
+    public void put(int index, T item){
+        array[index] = item;
+    }
+
+    public T get(int index){ return array[index];}
+
+    public  T[] rep (){ return array;}
+
+    public static void main(String[] args) {
+        GenericArrayComplex gai = new GenericArrayComplex<Integer>(10);
+        Object[] oa = gai.rep();
     }
 }
