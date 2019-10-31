361a4d03ea3dc530e2401fb7b01cb18ba6670397
liyuhang <xbypdcts@outlook.com>
2019-10-31 23:10:03 +0800

    泛型类型跟踪

---
+++ src/main/java/com/liyuhang/ThinkingInJava/Generic/GenericArrayWithTypeToken.java
@@ -0,0 +1,26 @@
+package com.liyuhang.ThinkingInJava.Generic;
+
+import java.lang.reflect.Array;
+
+public class GenericArrayWithTypeToken<T> {
+    private T[] array;
+    public GenericArrayWithTypeToken(Class<T> type, int sz){
+        array = (T[])Array.newInstance(type, sz);
+    }
+    public void put(int index, T item) {
+        array[index] = item;
+    }
+
+    public T get(int index){
+        return array[index];
+    }
+
+    public T[] rep() {
+        return array;
+    }
+
+    public static void main(String[] args) {
+        GenericArrayWithTypeToken<Integer> gai = new GenericArrayWithTypeToken<>(Integer.class, 10);
+        Integer[] ia = gai.rep();
+    }
+}
